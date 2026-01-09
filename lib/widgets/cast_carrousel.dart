cf17fda9a86783449bbdf5f51e273ee2bf5f5624
daaviddieeguez <daaviddieeguez@gmail.com>
2026-01-09 09:05:14 +0100

    CastCard finalizado

--- lib/widgets/cast_carrousel.dart
+++ lib/widgets/cast_carrousel.dart
@@ -7,8 +7,7 @@ class CastCarrousel extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      height: 100,
-      color: Colors.yellow,
+      height: 150,
       child: ListView.builder(
         itemCount: 10,
         scrollDirection: Axis.horizontal,
@@ -27,9 +26,29 @@ class _CastCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: 80,
-      height: 100,
+      height: 120,
       margin: EdgeInsets.symmetric(horizontal: 5),
-      color: Colors.blue,
+      child: Column(
+        children: [
+          //Imagen actor
+          ClipRRect(
+            borderRadius: BorderRadiusGeometry.circular(20),
+            child: FadeInImage(
+              placeholder: AssetImage('assets/no-image.jpg'), 
+              image: NetworkImage('https://m.media-amazon.com/images/M/MV5BMTY4Mjg0NjIxOV5BMl5BanBnXkFtZTcwMTM2NTI3MQ@@._V1_FMjpg_UX1000_.jpg'),
+              height: 100,
+              width: 80,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Text(
+            'actor.name Harrison Ford',
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.center,
+          )
+        ],
+      ),
     );
   }
 }
