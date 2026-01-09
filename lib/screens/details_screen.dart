7c64c00715c387fef9be4ea5662eaa53b30de9f0
daaviddieeguez <daaviddieeguez@gmail.com>
2026-01-09 08:48:17 +0100

    details_screen (Hasta descripcion)

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -10,12 +10,10 @@ class DetailsScreen extends StatelessWidget {
         slivers: [
           _CustomAppBar(),
           SliverList(
-            delegate: SliverChildListDelegate(
-              [Text('Hola')]
-            )
-          )
+            delegate: SliverChildListDelegate([_InfoPelicula(), _Overview()]),
+          ),
         ],
-      )
+      ),
     );
   }
 }
@@ -41,14 +39,20 @@ class _CustomAppBar extends StatelessWidget {
             child: Text(
               'Titulo pelicula',
               textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 16),
-            )
-          )
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
+            ),
+          ),
         ),
         centerTitle: true,
         background: FadeInImage(
           placeholder: AssetImage('assets/jar-loading.gif'),
-          image: NetworkImage('https://oracleoffilm.com/wp-content/uploads/1970/01/5-things-you-might-not-know-about-empire-strikes-back.jpg')
+          image: NetworkImage(
+            'https://oracleoffilm.com/wp-content/uploads/1970/01/5-things-you-might-not-know-about-empire-strikes-back.jpg',
+          ),
         ),
       ),
     );
@@ -69,14 +73,58 @@ class _InfoPelicula extends StatelessWidget {
             borderRadius: BorderRadiusGeometry.circular(20),
             child: FadeInImage(
               placeholder: AssetImage('assets/no-image.jpg'),
-              image: NetworkImage('https://pics.filmaffinity.com/star_wars_episode_v_the_empire_strikes_back-701818523-large.jpg'),
+              image: NetworkImage(
+                'https://pics.filmaffinity.com/star_wars_episode_v_the_empire_strikes_back-701818523-large.jpg',
+              ),
               height: 150,
             ),
           ),
+          SizedBox(width: 20),
           Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'movie.title',
+                style: Theme.of(context).textTheme.headlineMedium,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+              Text(
+                'movie.year',
+                style: Theme.of(context).textTheme.headlineSmall,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 1,
+              ),
 
-          )
+              Row(
+                children: [
+                  Icon(Icons.star_rate_sharp, size: 30, color: Colors.orange),
+                  Text(
+                    'movie.rate',
+                    style: Theme.of(context).textTheme.headlineSmall,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 1,
+                  ),
+                ],
+              ),
+            ],
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class _Overview extends StatelessWidget {
+  const _Overview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(20),
+      child: Text(
+        'movie.description. Nulla elit non enim duis ea exercitation sit. Mollit exercitation aute ullamco in non cupidatat sint. Tempor proident id culpa ex magna nulla mollit irure dolor. Qui officia Lorem anim eu sint quis aliqua sint commodo eiusmod esse.',
+        textAlign: TextAlign.justify,
       ),
     );
   }
